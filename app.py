42cd14ab88e8d1b877dc912a24ca14dd08858897
2021gonzalezgrobapedroj-hue <2021gonzalezgrobapedroj@colegioubaescobar.gob.ar>
2026-07-08 14:05:21 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,26 @@
- %%writefile app.py
+from flask import Flask, render_template, request, redirect
+from gastos import agregar_gasto, obtener_gastos, calcular_total
+
+app = Flask(__name__)
+
+@app.route("/")
+def inicio():
+    gastos = obtener_gastos()
+    total = calcular_total()
+    return render_template(
+        "index.html",
+        gastos=gastos,
+        total=total
+    )
+
+@app.route("/agregar", methods=["POST"])
+def agregar():
+    descripcion = request.form["descripcion"]
+    monto = float(request.form["monto"])
+
+    agregar_gasto(descripcion, monto)
+
+    return redirect("/")
+
+if __name__ == "__main__":
+    app.run(debug=True)
